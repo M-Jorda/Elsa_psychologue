39c023d2aa648b4d52f34216866f8524110fc3f6
Melchior Gibson <jorda.j.fr@gmail.com>
2024-04-10 11:30:17 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+// ========= HEADER ==========
+
+const nav = document.querySelector(".nav");
+const burger = document.getElementById("burger");
+const banner = document.querySelector(".img");
+let navOpened = false;
+
+function openCloseNav(e) {
+  if (!nav.classList.contains("activeHeader")) {
+    nav.classList.add("activeHeader");
+    navOpened = true;
+  } else {
+    nav.classList.remove("activeHeader");
+    navOpened = false;
+  }
+}
+
+document.addEventListener("click", function (e) {
+  if (navOpened && !nav.contains(e.target) && !burger.contains(e.target)) {
+    nav.classList.remove("activeHeader");
+    navOpened = false;
+  }
+});
+
+burger.onclick = openCloseNav;
+
+// ========= FOOTER ==========
+
+const footer = document.querySelector(".footer");
+const arrowFooter = document.querySelector(".arrows");
+
+function openCloseFooter() {
+  footer.classList.toggle("activeFooter");
+  arrowFooter.classList.toggle("arrowDownFooter");
+  arrowFooter.classList.toggle("arrowUpFooter");
+  1199;
+}
+
+arrowFooter.onclick = openCloseFooter;
+
+// ========= PAGES ==========
+
+const links = document.querySelectorAll(".link");
+
+links.forEach((link) => {
+  link.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+    nav.classList.remove("activeHeader");
+  });
+});
+
+function chargerContenu(url) {
+  fetch(url) // Charger le fichier HTML externe
+    .then((response) => response.text()) // Convertir la réponse en texte
+    .then((html) => {
+      document.querySelector("main").innerHTML = html; // Ajouter le contenu au conteneur
+    })
+    .catch((error) =>
+      console.error("Erreur de chargement du contenu :", error)
+    );
+}
+
+// ========= MEDIA-QUERIES ==========
+
+const header = document.querySelector("header");
+const navi = document.querySelector("nav");
+
+const mediaMd = window.matchMedia("(min-width: 768px)");
+
+function changeClassOnMd() {
+  if (!mediaMd.matches) {
+    nav.classList.add("position-absolute");
+    navi.classList.add("d-flex", "flex-column-reverse");
+    header.classList.add("fixed-bottom", "d-flex", "flex-column-reverse");
+    header.classList.remove("position-absolute");
+  } else {
+    header.classList.remove("fixed-bottom", "d-flex", "flex-column-reverse");
+    navi.classList.add("d-flex", "flex-column-reverse");
+    header.classList.add("position-absolute");
+    nav.classList.remove("position-absolute");
+  }
+}
+
+mediaMd.addEventListener("change", changeClassOnMd);
+
+window.onload = function () {
+  chargerContenu("./htmls/home.html");
+  changeClassOnMd();
+};
---
+++ schema.org.js
@@ -0,0 +1,30 @@
+{
+  "@context" = "https://schema.org",
+  "@type" = "Psychologist",
+  "name" = "Elsa Destenabes",
+  "address" = {
+    "@type": "PostalAddress",
+    "streetAddress": "4 rue Oudinot",
+    "addressLocality": "Paris",
+    "addressRegion": "ile-de-France",
+    "postalCode": "7500",
+    "addressCountry": "France"
+  },
+  "telephone"= "06.43.29.84.35",
+  "email"= "elsadestenabes.psy@gmail.com",
+  "url"= "https://elsapsy-835fb.web.app/",
+  "openingHoursSpecification" = [
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday"
+      ],
+      "opens": "09:00",
+      "closes": "18:00"
+    }
+  ]
+}
